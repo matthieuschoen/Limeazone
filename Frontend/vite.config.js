415f9ab6e39b50ce0efb5bf91fd9ace8265afbd8
Matthieu Schoen <matthieu.schoen@epitech.eu>
2025-08-24 01:22:15 +0200

    Update Vite configuration base path for deployment

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 export default defineConfig({
-  base: '/Limeazone/', // Remplacez par le nom de votre repo GitHub
+  base: '/', // Remplacez par le nom de votre repo GitHub
   plugins: [
     vue({
       template: { transformAssetUrls }
